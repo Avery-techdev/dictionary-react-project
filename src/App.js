1b03aeba5683b9ab3731ac50b340c38f21d98e90
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-04-06 19:25:11 +0200

    project setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import dictionary from './dictionary.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={dictionary} className="App-logo" alt="logo" />
+        
+          ictionary
+  
+      </header>
+    </div>
+  );
+}
