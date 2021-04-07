a1e52be59bc63a89a7c8c896927a0e4860f239f2
Avery-techdev <77972529+Avery-techdev@users.noreply.github.com>
2021-04-07 18:38:54 +0200

    implemented search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import dictionary from './dictionary.svg';
+import Dictionary from "./Dictionary";
 import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className="App Container">
       <header className="App-header">
         <img src={dictionary} className="App-logo" alt="logo" />
-        
-          ictionary
-  
+        <span>
+         ictionary
+        </span>
       </header>
+      <main>
+        <Dictionary />
+      </main>
       <footer> 
         open source <a href="https://github.com/Avery-techdev/dictionary-react-project">code</a> by Annabell Hauschild
       </footer> 
---
+++ src/Dictionary.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+  event.preventDefault();
+  alert(`Searching for ${keyword}`)
+  }
+
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value)
+  }
+
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" autoFocus={true} className="form-control" onChange={handleKeywordChange}/>
+      </form>
+    </div>
+  )
+}
